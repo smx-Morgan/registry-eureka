deca2a26588c484bbd4fd70ccb1009f00c4dd522
smx_Moragn <1048492656@qq.com>
2024-10-13 16:34:54 +0800

    softly connected to cwgo-pkg

--- registry/registry.go
+++ registry/registry.go
@@ -16,19 +16,10 @@ package registry
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"net"
-	"strconv"
-	"sync"
+	eurekakitex "github.com/cloudwego-contrib/cwgo-pkg/registry/eureka/eurekakitex/registry"
 	"time"
 
-	"github.com/cloudwego/kitex/pkg/discovery"
-	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/pkg/registry"
-	"github.com/hudl/fargo"
-	"github.com/kitex-contrib/registry-eureka/constants"
-	"github.com/kitex-contrib/registry-eureka/entity"
 )
 
 type eurekaHeartbeat struct {
@@ -36,146 +27,7 @@ type eurekaHeartbeat struct {
 	instanceKey string
 }
 
-type eurekaRegistry struct {
-	eurekaConn       *fargo.EurekaConnection
-	heatBeatInterval time.Duration
-	lock             *sync.RWMutex
-	registryIns      map[string]*eurekaHeartbeat
-}
-
 // NewEurekaRegistry creates a eureka registry.
 func NewEurekaRegistry(servers []string, heatBeatInterval time.Duration) registry.Registry {
-	conn := fargo.NewConn(servers...)
-	return &eurekaRegistry{
-		eurekaConn:       &conn,
-		heatBeatInterval: heatBeatInterval,
-		registryIns:      make(map[string]*eurekaHeartbeat),
-		lock:             &sync.RWMutex{},
-	}
-}
-
-// Register register a server with given registry info.
-func (e *eurekaRegistry) Register(info *registry.Info) error {
-	instance, err := e.eurekaInstance(info)
-	if err != nil {
-		return err
-	}
-
-	instanceKey := fmt.Sprintf("%s:%s", info.ServiceName, info.Addr.String())
-
-	e.lock.RLock()
-	_, ok := e.registryIns[instanceKey]
-	e.lock.RUnlock()
-	if ok {
-		return fmt.Errorf("instance{%s} already registered", instanceKey)
-	}
-
-	if err = e.eurekaConn.RegisterInstance(instance); err != nil {
-		return err
-	}
-
-	ctx, cancel := context.WithCancel(context.Background())
-	go e.heartBeat(ctx, instance)
-
-	e.lock.Lock()
-	defer e.lock.Unlock()
-	e.registryIns[instanceKey] = &eurekaHeartbeat{
-		instanceKey: instanceKey,
-		cancel:      cancel,
-	}
-
-	return nil
-}
-
-// Deregister deregister a server with given registry info.
-func (e *eurekaRegistry) Deregister(info *registry.Info) error {
-	instance, err := e.eurekaInstance(info)
-	if err != nil {
-		return err
-	}
-
-	instanceKey := fmt.Sprintf("%s:%s", info.ServiceName, info.Addr.String())
-
-	e.lock.RLock()
-	insHeartbeat, ok := e.registryIns[instanceKey]
-	e.lock.RUnlock()
-	if !ok {
-		return fmt.Errorf("instance{%s} has not registered", instanceKey)
-	}
-
-	if err = e.eurekaConn.DeregisterInstance(instance); err != nil {
-		return err
-	}
-
-	e.lock.Lock()
-	insHeartbeat.cancel()
-	delete(e.registryIns, instanceKey)
-	e.lock.Unlock()
-
-	return nil
-}
-
-func (e *eurekaRegistry) eurekaInstance(info *registry.Info) (*fargo.Instance, error) {
-	if info == nil {
-		return nil, ErrNilInfo
-	}
-	if info.Addr == nil {
-		return nil, ErrNilAddr
-	}
-	if len(info.ServiceName) == 0 {
-		return nil, ErrEmptyServiceName
-	}
-
-	host, portStr, err := net.SplitHostPort(info.Addr.String())
-	if err != nil {
-		return nil, err
-	}
-	if host == "" || host == "::" {
-		return nil, ErrMissIP
-	}
-
-	port, err := strconv.ParseInt(portStr, 10, 64)
-	if err != nil {
-		return nil, err
-	}
-	if port <= 0 {
-		return nil, ErrMissPort
-	}
-
-	if info.Weight == 0 {
-		info.Weight = discovery.DefaultWeight
-	}
-
-	meta, err := json.Marshal(&entity.RegistryEntity{Weight: info.Weight, Tags: info.Tags})
-	if err != nil {
-		return nil, err
-	}
-	instanceKey := fmt.Sprintf("%s:%s", info.ServiceName, info.Addr.String())
-	instance := &fargo.Instance{
-		HostName:       instanceKey,
-		InstanceId:     instanceKey,
-		App:            info.ServiceName,
-		IPAddr:         host,
-		Port:           int(port),
-		Status:         fargo.UP,
-		DataCenterInfo: fargo.DataCenterInfo{Name: fargo.MyOwn},
-	}
-
-	instance.SetMetadataString(constants.Meta, string(meta))
-	return instance, nil
-}
-
-func (e *eurekaRegistry) heartBeat(ctx context.Context, ins *fargo.Instance) {
-	ticker := time.NewTicker(e.heatBeatInterval)
-	for {
-		select {
-		case <-ctx.Done():
-			ticker.Stop()
-			return
-		case <-ticker.C:
-			if err := e.eurekaConn.HeartBeatInstance(ins); err != nil {
-				klog.Errorf("heartBeat error,err=%+v", err)
-			}
-		}
-	}
+	return eurekakitex.NewEurekaRegistry(servers, heatBeatInterval)
 }
--- resolver/resolver.go
+++ resolver/resolver.go
@@ -15,17 +15,9 @@
 package resolver
 
 import (
-	"context"
-	"encoding/json"
-	"errors"
-	"fmt"
-
+	eurekaresolver "github.com/cloudwego-contrib/cwgo-pkg/registry/eureka/eurekakitex/resolver"
 	"github.com/cloudwego/kitex/pkg/discovery"
-	"github.com/cloudwego/kitex/pkg/kerrors"
-	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/hudl/fargo"
-	"github.com/kitex-contrib/registry-eureka/constants"
-	"github.com/kitex-contrib/registry-eureka/entity"
 )
 
 // eurekaResolver is a resolver using eureka.
@@ -35,67 +27,5 @@ type eurekaResolver struct {
 
 // NewEurekaResolver creates a eureka resolver.
 func NewEurekaResolver(servers []string) discovery.Resolver {
-	conn := fargo.NewConn(servers...)
-	return &eurekaResolver{eurekaConn: &conn}
-}
-
-// Target implements the Resolver interface.
-func (r *eurekaResolver) Target(ctx context.Context, target rpcinfo.EndpointInfo) string {
-	return target.ServiceName()
-}
-
-// Resolve implements the Resolver interface.
-func (r *eurekaResolver) Resolve(ctx context.Context, desc string) (discovery.Result, error) {
-	application, err := r.eurekaConn.GetApp(desc)
-	if err != nil {
-		if errors.As(err, &fargo.AppNotFoundError{}) {
-			return discovery.Result{}, kerrors.ErrNoMoreInstance
-		}
-		return discovery.Result{}, err
-	}
-
-	eurekaInstances := application.Instances
-	instances, err := r.instances(eurekaInstances)
-	if err != nil {
-		return discovery.Result{}, err
-	}
-
-	return discovery.Result{CacheKey: desc, Cacheable: true, Instances: instances}, nil
-}
-
-// Diff implements the Resolver interface.
-func (r *eurekaResolver) Diff(cacheKey string, prev, next discovery.Result) (discovery.Change, bool) {
-	return discovery.DefaultDiff(cacheKey, prev, next)
-}
-
-// Name implements the Resolver interface.
-func (r *eurekaResolver) Name() string {
-	return constants.Eureka
-}
-
-func (r *eurekaResolver) instances(instances []*fargo.Instance) ([]discovery.Instance, error) {
-	res := make([]discovery.Instance, 0, len(instances))
-	for _, instance := range instances {
-		dInstance, err := r.instance(instance)
-		if err != nil {
-			return nil, err
-		}
-		res = append(res, dInstance)
-	}
-
-	return res, nil
-}
-
-func (r *eurekaResolver) instance(instance *fargo.Instance) (discovery.Instance, error) {
-	var dInstance discovery.Instance
-	var e entity.RegistryEntity
-	meta, err := instance.Metadata.GetString(constants.Meta)
-	if err != nil {
-		return dInstance, err
-	}
-	if err = json.Unmarshal([]byte(meta), &e); err != nil {
-		return dInstance, err
-	}
-
-	return discovery.NewInstance(constants.TCP, fmt.Sprintf("%s:%d", instance.IPAddr, instance.Port), e.Weight, e.Tags), nil
+	return eurekaresolver.NewEurekaResolver(servers)
 }
